c9ad1083416ad0c8c52631eab2f1648922362e65
muhammadjonr <mrahmonaliev22032007@gmail.com>
2024-01-13 09:45:27 +0500

    posts

--- src/assets/Users.jsx
+++ src/assets/Users.jsx
@@ -1,20 +1,41 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const Users = () => {
-    const [users, setUsers] = useState([])
-    const usersData = async () =>{
-        try {
-            const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
-            const data = await res.json();
-            setUsers(data)
-        } catch (error) {
-            console.log(error)
-        }
+  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const [albums, setAlbums] = useState([]);
+  const usersData = async () => {
+    try {
+      const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+      const data = await res.json();
+      setUsers(data);
+    } catch (error) {
+      console.log(error);
     }
-    
-    useEffect(() => {
-      usersData();
-    }, []);
+  };
+  const getPosts = async (id) => {
+    try {
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts?userId=${id}`
+      );
+      const data = await res.json();
+      setPosts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getTodos = () => {
+    console.log("hello");
+  };
+  const getAlbums = () => {
+    console.log("hello");
+  };
+
+  useEffect(() => {
+    usersData();
+  }, []);
 
   return (
     <div className="api">
@@ -24,15 +45,32 @@ const Users = () => {
           <div className="user" key={user.id}>
             <h3>{user.name}</h3>
             <span>{user.username}</span>
-            <p>Email:{user.email}</p>
-            <p>Website:{user.website}</p>
-            <p>Address:{user.address.street}</p>
-            <p>Phone:{user.phone}</p>
+            <p>Email: {user.email}</p>
+            <p>Website: {user.website}</p>
+            <p>Address: {user.address.street}</p>
+            <p>Phone: {user.phone}</p>
+            <div className="user_btn">
+              <button onClick={() => getPosts(user.id)}>Posts</button>
+              <button onClick={() => getTodos(user.id)}>Todos</button>
+              <button onClick={() => getAlbums(user.id)}>Album</button>
+            </div>
           </div>
         ))}
       </div>
+      <div className="blog_posts">
+        <div className="container">
+            <h1>Posts</h1>
+          <div className="posts">
+            {posts.map((post) => {
+                <div className="post" key={post.id}>
+                    <h4>{post.title}</h4>
+                </div>
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default Users
+export default Users;
